46d98e013768a490a3cd7ebeaa2a75acddf6894b
sophia <15851587+sophiavf@users.noreply.github.com>
2023-03-06 22:44:22 +0100

    Added new modules for each pages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import "./style.css";
+
+import contactPage from "./contact";
+import headerImage from "./chris-liverani-unsplash.jpg";
 console.log("Hello");
+
+function component() {
+	const element = document.createElement("div");
+
+	// use your function!
+	element.textContent = myName("Cody");
+	return element;
+}
+
+document.body.appendChild(component());
